e7d4cb97fbe48a07a93bd265f8d0ed8cca49eb3c
Joshua Stimpert <jStimpert0430@starkstate.net>
2019-02-26 12:37:09 -0500

    Uploaded Screenshot Tool Project C#

---
+++ Csharp/jStimpert-ScreenCapFinal/jStimpert-ScreenCapFinal/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace jStimpert_ScreenCapFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Capture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(649, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1, 1);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouse_Move);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(793, 463);
+            this.panel1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(730, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Click and drag over captured image to crop; Click again to restore original scree" +
+    "nshot";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(529, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Open File on Save";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 516);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "jStimpert0430 - Screen Capture Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Csharp/jStimpert-ScreenCapFinal/jStimpert-ScreenCapFinal/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+
+
+namespace jStimpert_ScreenCapFinal
+{
+    public partial class Form1 : Form
+    {
+        Rectangle selectionRect = new Rectangle();
+        Color penColor = Color.Red;
+        bool openFileOnSave = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Method to capture screen image based on provided location and size. If no arguments are provided method will take screenshot of entire monitor space.
+        public void CaptureScreen(int xValue = 0, int yValue = 0, int height = 0, int width = 0) {
+            Rectangle captureArea = new Rectangle();
+            //Force garbage collection each capture to avoid loading memory with garbage.
+            System.GC.Collect();
+            System.GC.WaitForPendingFinalizers();
+            //if sent a rectangle, use those values. Otherwise, set capture rectangle to entire screenspace
+            if (height != 0 && width != 0)
+            {
+                captureArea = new Rectangle(xValue, yValue, height, width);
+            }
+            else
+            {
+                captureArea = Screen.AllScreens[0].Bounds;
+            }
+            //Create bitmap of same size as rectangle
+            Bitmap screenshot = new Bitmap(captureArea.Width, captureArea.Height, PixelFormat.Format32bppArgb);
+            //Create Graphics object and use data from bitmap
+            Graphics screenshotGrabber = Graphics.FromImage(screenshot);
+            //Copy image from screen to graphics object
+            screenshotGrabber.CopyFromScreen(captureArea.Location.X, captureArea.Location.Y, 0, 0, captureArea.Size);
+            //Show image in picture box
+            pictureBox1.Image = screenshot;
+            screenshotGrabber.Dispose();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Cursor = Cursors.Cross;
+            CaptureScreen();
+        }
+
+
+        //Clear image in picturebox
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //If there is a picture to be saved, open file dialog and get location
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog saveImageDialog = new SaveFileDialog();
+                saveImageDialog.Filter = "Jpeg Image|*.jpg";
+                saveImageDialog.Title = "Save a Screenshot";
+                saveImageDialog.ShowDialog();
+                //Once a valid file name is recieved, open IO stream to the file, send image data in jpeg format, and close filestream.
+                if (saveImageDialog.FileName != "")
+                {
+                    System.IO.FileStream fs = (System.IO.FileStream)saveImageDialog.OpenFile();
+                    pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    fs.Close();
+                    if (openFileOnSave == true)
+                        System.Diagnostics.Process.Start(saveImageDialog.FileName.ToString());
+                    else
+                        MessageBox.Show("Save Successful", "File Saved");
+                }
+            }
+            //Show error if no screenshot has been taken yet.
+            else
+                MessageBox.Show("Must first capture a screenshot to save.","Error: No Screenshot");
+        }
+
+
+        private void mouse_Move(object sender, MouseEventArgs e)
+        {
+            //If the left mouse button is being held while moving over the pictureBox, keep track of location and compare against location from initial click -- then build rectangle based on information
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = Math.Min(selectionRect.Location.X, e.X);
+                int y = Math.Min(selectionRect.Location.Y, e.Y);
+                int width = Math.Max(selectionRect.Location.X, e.X) - Math.Min(selectionRect.Location.X, e.X);
+                int height = Math.Max(selectionRect.Location.Y, e.Y) - Math.Min(selectionRect.Location.Y, e.Y);
+                selectionRect = new Rectangle(x, y, width, height);
+                Refresh();
+            }
+        
+        }
+
+
+        //Paint method to paint selection box into pictureBox
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(penColor, 1))
+            {
+                e.Graphics.DrawRectangle(pen, selectionRect);
+            }
+        }
+
+
+        //set initial click location to build rectangle with.
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e){
+            pictureBox1.Cursor = Cursors.Cross;
+            selectionRect.Location = e.Location;
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) { }
+
+
+        //Once mouse is released, take screenshot of cropped area and reprint. Since the rectangle can only be over the screenshot, there is no reason to use the old image. 
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Set pen color to trasparent and clear old image data to avoid it showing up in screenshot
+            penColor = Color.Transparent;
+            pictureBox1.Image = null;
+            Refresh();
+            //Capture area inside rectangle specified by mouse move event
+            CaptureScreen(selectionRect.X, selectionRect.Y, selectionRect.Width, selectionRect.Height);
+            //Reset pen color, delete rectangle, and redraw image.
+            pictureBox1.Cursor = Cursors.Default;
+            penColor = Color.Red;
+            selectionRect = new Rectangle();
+            Refresh();
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                openFileOnSave = true;
+            else
+                openFileOnSave = false;
+        }
+    }
+}
